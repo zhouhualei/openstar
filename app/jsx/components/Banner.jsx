2773ea906dd372279dc31d04a189be5811fa3480
warren <zhouhualei@gmail.com>
2015-04-06 16:23:00 +0800

    change layout

--- app/jsx/components/Banner.jsx
+++ app/jsx/components/Banner.jsx
@@ -22,25 +22,8 @@ module.exports = React.createClass({
           </div>
           <div className="collapse navbar-collapse">
             <ul className="nav navbar-nav navbar-right">
-              <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown
-                  <span className="caret"></span>
-                </a>
-                <ul className="dropdown-menu" role="menu">
-                  <li>
-                    <a href="#">Action</a>
-                  </li>
-                  <li>
-                    <a href="#">Another action</a>
-                  </li>
-                  <li>
-                    <a href="#">Something else here</a>
-                  </li>
-                  <li className="divider"></li>
-                  <li>
-                    <a href="#">Separated link</a>
-                  </li>
-                </ul>
+              <li>
+                <a href="#">Link</a>
               </li>
             </ul>
           </div>
